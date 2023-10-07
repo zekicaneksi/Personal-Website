a33adc199809d0120ab5802a656b25741532348d
Zeki Can EKSI <zekicaneksi@gmail.com>
2023-10-07 21:44:57 +0300

    made the loading screen

--- personal-website/src/context/loading_screen.tsx
+++ personal-website/src/context/loading_screen.tsx
@@ -4,6 +4,8 @@ import { createContext, useState } from 'react';
 import { useRouter } from 'next/navigation'
 import styles from './loading_screen.module.css';
 
+import SpiderIcon from '@/icon/Spider'
+
 interface LoadingScreenContextType {
   isLoading: boolean;
   setIsLoading: Function;
@@ -23,9 +25,46 @@ const ContextProvider = (props: {children : React.ReactNode}) => {
         setTimeout(() => {router.push(url)}, 3000)
     }
 
+    const spiderDropCss : React.CSSProperties = {
+        position: 'absolute',
+        top: isLoading ? '65vh' : '0vh',
+        left: '50vw',
+        transform: 'translate(-50%, -100%)',
+        transition: 'top 2.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0s',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+
+    const top_spiderLineStyleCss : React.CSSProperties = {
+        width: '10px',
+        height: '65vh',
+        backgroundColor: 'rgb(73, 125, 73)',
+        transition: 'border-radius 3s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0s',
+        borderRadius: isLoading ? '100%' : '1%',
+    }
+
+    const spiderCss : React.CSSProperties = {
+        transform: 'rotate(0deg)',
+    }
+
+    function getCurtainCss(direction : 'left' | 'right') : React.CSSProperties {
+        return {
+            background: `repeating-linear-gradient(${direction === 'left' ? '110deg' : '70deg'}, #680303, #640202 10%)`
+        }
+    }
+
   return (
     <Context.Provider value={{isLoading, setIsLoading, navigate}}>
-        <div style={{width: (isLoading ? '100vw' : '0vw')}} className={styles.loadingScreen}></div>
+        <div className={styles.loadingScreenContainer}>
+           <div style={{width: (isLoading ? '50vw' : '0vw'), left: '0vw', ...(getCurtainCss('left')) }} className={`${styles.curtain}`}></div>
+           <div style={{width: (isLoading ? '50vw' : '0vw'), left: (isLoading ? '50vw' : '100vw'), ...(getCurtainCss('right')) }} className={`${styles.curtain}`}></div>
+            <div style={spiderDropCss}>
+                <div style={top_spiderLineStyleCss}></div>
+                <SpiderIcon style={spiderCss} fill={'#2B572B'}/>
+            </div>
+        </div>
         {props.children}
     </Context.Provider>
   )
