c05eed89c26478b6a240b486a96d17087be46571
Zeki Can EKSI <zekicaneksi@gmail.com>
2023-10-10 23:12:07 +0300

    layout of project cubes are done

--- personal-website/src/app/projects/page.tsx
+++ personal-website/src/app/projects/page.tsx
@@ -1,21 +1,49 @@
-'use client'
+"use client";
 
-import { useContext, useEffect } from 'react';
-import { Context as LoadingScreenContext } from '@/context/loading_screen';
+import { useContext, useEffect } from "react";
+import { Context as LoadingScreenContext } from "@/context/loading_screen";
 
+import styles from "./page.module.css";
 
-export default function Page(){
-    
-    const {setIsLoading, navigate} = useContext(LoadingScreenContext) || {};
-    
-    useEffect(() => {
-        if(setIsLoading !== undefined) setIsLoading(false);
-    },[setIsLoading])
+const ProjectCube = () => {
+  return (
+    <div className={styles.cubeDiv}>
+      <p>Hello</p>
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <h1 style={{color: 'red'}}>hello world</h1>
-            <h1 style={{color: 'red'}} onClick={() => {if(navigate !== undefined) navigate('/')}}>click on me to go back</h1>
-        </div>
-    )
+export default function Page() {
+  const { setIsLoading, navigate } = useContext(LoadingScreenContext) || {};
+
+  useEffect(() => {
+    if (setIsLoading !== undefined) setIsLoading(false);
+  }, [setIsLoading]);
+
+  return (
+    <div>
+      <div>
+        <h1
+          style={{ color: "red" }}
+          onClick={() => {
+            if (navigate !== undefined) navigate("/");
+          }}
+        >
+          click on me to go back
+        </h1>
+      </div>
+      <div
+        style={{
+          width: "100vw",
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+        }}
+      >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((key) => (
+          <ProjectCube key={key} />
+        ))}
+      </div>
+    </div>
+  );
 }
